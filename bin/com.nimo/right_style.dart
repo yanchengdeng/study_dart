b0aef7c1f2bc500a9fe6ae435ce25d11e9034ea1
yanchengdeng <dycjavaee@163.com>
2022-04-01 16:30:29 +0800

    dart 最佳写法实践

---
+++ bin/com.nimo/right_style.dart
@@ -0,0 +1,56 @@
+
+import 'dart:math';
+
+///具体参考 [https://github.com/dart-lang/dart_style]
+
+
+void main(){
+  error(null);
+
+  var optionalThing = OptionalThing();
+  // userIf()
+
+  var numbers = [1,1.2,2];
+  var ints = numbers.whereType<int>();
+
+  var stuff = <dynamic>[1,2];
+  var ints1 = List<int>.from(stuff);
+  print(ints1);
+
+  var reipcoals = stuff.map<double>((n) => 1/n);
+  print(reipcoals);
+
+
+}
+
+void error(String? msg){
+  print(msg ?? "msg is null");
+}
+class OptionalThing{
+  bool? isEnabled;
+}
+
+///倾向于使用  ??
+void userIf(OptionalThing? optionalThing){
+  //good
+  if(optionalThing?.isEnabled ?? false){
+
+  }
+  //bad
+  if(optionalThing?.isEnabled == false){
+
+  }
+}
+
+class Circle {
+  double radius;
+  double area;
+  double circumference;
+
+  Circle(this.radius)
+      : area = pi * radius * radius,
+        circumference = pi * 2.0 * radius;
+
+
+}
+
